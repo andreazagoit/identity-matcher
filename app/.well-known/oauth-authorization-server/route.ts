cdb5892da4bd7824cab5c88773712a43054fd494
Andrea Zago <info@andreazago.it>
2026-02-17 02:22:17 +0100

    Remove obsolete OAuth authorization server route file to streamline codebase and eliminate unused components.

--- app/.well-known/oauth-authorization-server/route.ts
+++
@@ -1,6 +0,0 @@
-import { auth } from "@/lib/auth";
-import { oauthProviderAuthServerMetadata } from "@better-auth/oauth-provider";
-
-const handler = oauthProviderAuthServerMetadata(auth);
-
-export const GET = handler;
